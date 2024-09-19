f1b209a3ea81011e3a527305c7935f4598213959
0x0Louis <louis@joepegs.com>
2024-09-19 03:36:15 +0200

    Update flags

--- src/libraries/Flags.sol
+++ src/libraries/Flags.sol
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// flags = [zeroForOne? | callback? | transferTaxToken? | (id >> 16)]
-// Each flag is a bit in the flags variable
-// The id is the last 16 bits of the flags variable
+// flags = [zeroForOne? | callback? | id]
+// The zeroForOne flag is the first bit of the flags variable
+// The callback flag is the second bit of the flags variable
+// The id is the last 8 bits of the flags variable
 library Flags {
     uint256 internal constant ZERO_FOR_ONE = 1;
     uint256 internal constant CALLBACK = 2;
-    uint256 internal constant TRANSFER_TAX_TOKEN = 4;
-    uint256 internal constant ID_OFFSET = 16;
-    uint256 internal constant ID_MASK = 0xffff;
 
-    uint256 internal constant UNISWAP_V2_ID = 1;
-    uint256 internal constant TRADERJOE_LB0_ID = 2;
-    uint256 internal constant TRADERJOE_LB12_ID = 3; // v2.1 and v2.2 have the same ABI for swaps
-    uint256 internal constant UNISWAP_V3_ID = 4;
+    uint256 internal constant ID_OFFSET = 8;
+    uint256 internal constant ID_MASK = 0xff00;
+
+    uint256 internal constant UNISWAP_V2_ID = 1 << 8;
+    uint256 internal constant TRADERJOE_LEGACY_LB_ID = 2 << 8;
+    uint256 internal constant TRADERJOE_LB_ID = 3 << 8; // v2.1 and v2.2 have the same ABI for swaps
+    uint256 internal constant UNISWAP_V3_ID = 4 << 8;
 
     function id(uint256 flags) internal pure returns (uint256 idx) {
-        return (flags >> ID_OFFSET) & ID_MASK;
+        return flags & ID_MASK;
     }
 
     function zeroForOne(uint256 flags) internal pure returns (bool) {
@@ -26,9 +27,5 @@ library Flags {
 
     function callback(uint256 flags) internal pure returns (bool) {
         return flags & CALLBACK == CALLBACK;
-    }
-
-    function transferTaxToken(uint256 flags) internal pure returns (bool) {
-        return flags & TRANSFER_TAX_TOKEN == TRANSFER_TAX_TOKEN;
     }
 }
