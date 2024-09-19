00d951bea1d056d30d3ee4a2efa225c20e0c367f
0x0Louis <louis@joepegs.com>
2024-09-19 03:36:26 +0200

    Update PackedLibrary

--- src/libraries/PackedRoute.sol
+++ src/libraries/PackedRoute.sol
@@ -1,59 +1,139 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// [amount, pair01, token1, flags01, pair12, token2, flags02, ..., pairN-1N, tokenN, flagsN-1N]
+/**
+ * @title RouteDecoder
+ * @dev Helper library to decode packed route data
+ * Route data is a byte array following the format:
+ * [nb tokens]
+ * [isTransferTax, token0, token1, token2, ..., tokenN-1, tokenN]
+ * [{pairAB, percentAB, flagsAB, tokenA_id, tokenB_id}, {pairBC, percentBC, flagsBC, tokenB_id, tokenC_id}, ...]
+ *
+ * The number of tokens is encoded on the first byte, it must be less or equal to 255.
+ * The isTransferTax is a boolean flag that indicates if the token0 is a transfer tax token.
+ * The tokens are encoded as 20 bytes addresses.
+ *     The token0 must be the tokenIn and the tokenN must be the tokenOut.
+ * The pairs are encoded as 20 bytes addresses.
+ * The percent is a 16 bits unsigned integer, it must be less or equal to 10 000 (100%).
+ *     It represents the percentage of the remaining amount use for the swap. Therefore, the last swap must be 100%.
+ *     Thus, the sum of all percents will exceed 100% if there are more than 1 swap.
+ * The flags are encoded as 16 bits unsigned integer. They contain the dex id and information for the swap. See the
+ *     Flags library for more information.
+ * The token ids are encoded as 8 bits unsigned integer. They must match the id of the token in the token list.
+ * All the values are packed in a bytes array each time using the least amount of bytes possible (in solidity, use abi.encodePacked).
+ *
+ * Example:
+ *
+ *                          WETH
+ *                  0.8       |     0.2
+ *                   -----------------
+ *                   |               |
+ *         UNIV3-WETH/WAVAX     LB2.1-WETH/USDC
+ *                   |               |
+ *                 WAVAX             |
+ *            0.3    |      0.7      |
+ *            -----------------      |
+ *            |               |      |
+ *            |    UNIV2-WAVAX/USDC  |
+ *            |               |      |
+ *            |               --------
+ *            |                    |
+ *            |                  USDC
+ * LB2.0-WAVAX/USDT         0.4    |     0.6
+ *            |              --------------
+ *            |              |            |
+ *            |              |   UNIV3-BTC/USDC
+ *            |              |            |
+ *            |              |           BTC
+ *            |    LB2.2-USDC/USDT        |
+ *            |              |   UNIV2-BTC/USDT
+ *            |              |            |
+ *            -----------------------------
+ *                           |
+ *                          USDT
+ *
+ * Encoding:
+ * Here:
+ *              0     1     2     3     4
+ * [5][false, WETH, WAVAX, USDC, BTC, USDT] // 5 tokens, WETH is not a transfer tax token
+ * {UNIV3-WETH/WAVAX,  8000, UNIV3_ID | CALLBACK | ZERO_FOR_ONE, 0, 1}
+ * {LB2.1-WETH/USDC,  10000,            LB2_1_ID | ZERO_FOR_ONE, 0, 2}
+ * {UNIV2-WAVAX/USDC,  7000,            UNIV2_ID | ZERO_FOR_ONE, 1, 2}
+ * {UNIV3-BTC/USDC,    6000, UNIV3_ID | CALLBACK | ZERO_FOR_ONE, 1, 3}
+ * {UNIV2-BTC/USDT,   10000,            UNIV2_ID | ZERO_FOR_ONE, 3, 4}
+ * {LB2.2-WAVAX/USDT, 10000,            LB2_0_ID | ONE_FOR_ZERO, 1, 4}
+ * {LB2.1-USDC/USDT,  10000,            LB2_2_ID | ONE_FOR_ZERO, 2, 4}
+ */
 library PackedRoute {
     error PackedRoute__InvalidLength();
 
-    uint256 internal constant HEADER_SIZE = 16;
+    uint256 internal constant IS_TRANSFER_TAX_OFFSET = 1;
+    uint256 internal constant TOKENS_OFFSET = 2;
+    uint256 internal constant ROUTE_SIZE = 26;
+    uint256 internal constant ADDRESS_SIZE = 20;
 
-    function length(bytes calldata route) internal pure returns (uint256 l) {
-        l = route.length / 44;
-        if (route.length % 44 != 16) revert PackedRoute__InvalidLength();
-    }
+    uint256 internal constant IS_TRANSFER_TAX_SHIFT = 248;
+    uint256 internal constant ADDRESS_SHIFT = 96;
+    uint256 internal constant PERCENT_SHIFT = 80;
+    uint256 internal constant FLAGS_SHIFT = 64;
+    uint256 internal constant TOKEN_IN_SHIFT = 56;
+    uint256 internal constant TOKEN_OUT_SHIFT = 48;
+    uint256 internal constant UINT16_MASK = 0xffff;
+    uint256 internal constant UINT8_MASK = 0xff;
 
-    function start(bytes calldata route) internal pure returns (uint256 ptr, uint256 amount) {
+    function isTransferTax(bytes calldata route) internal pure returns (bool b) {
         assembly {
-            amount := shr(128, calldataload(route.offset))
-            ptr := 16
+            b := iszero(iszero(shr(IS_TRANSFER_TAX_SHIFT, calldataload(add(route.offset, IS_TRANSFER_TAX_OFFSET)))))
         }
     }
 
-    function next(bytes calldata route, uint256 ptr)
-        internal
-        pure
-        returns (uint256 nextPtr, address pair, address token, uint256 flags)
-    {
-        (pair, token, flags) = _get(route, ptr);
+    function token(bytes calldata route, uint256 id) internal pure returns (address t) {
+        assembly {
+            t := shr(ADDRESS_SHIFT, calldataload(add(route.offset, add(TOKENS_OFFSET, mul(id, ADDRESS_SIZE)))))
+        }
+    }
+
+    function start(bytes calldata route) internal pure returns (uint256 ptr, uint256 nbTokens, uint256 nbSwaps) {
+        assembly {
+            nbTokens := shr(248, calldataload(route.offset))
+        }
 
         unchecked {
-            nextPtr = ptr + 44;
+            uint256 length = route.length;
+
+            ptr = TOKENS_OFFSET + nbTokens * ADDRESS_SIZE;
+            uint256 swapLength = length - ptr;
+
+            nbSwaps = swapLength / ROUTE_SIZE;
+            if (length < ptr || swapLength % ROUTE_SIZE != 0) revert PackedRoute__InvalidLength();
         }
     }
 
-    function previous(bytes calldata route, uint256 ptr)
+    function next(bytes calldata route, uint256 ptr) internal pure returns (uint256 nextPtr, bytes32 value) {
+        assembly {
+            value := calldataload(add(route.offset, ptr))
+            nextPtr := add(ptr, ROUTE_SIZE)
+        }
+    }
+
+    function previous(bytes calldata route, uint256 ptr) internal pure returns (uint256 previousPtr, bytes32 value) {
+        assembly {
+            previousPtr := sub(ptr, ROUTE_SIZE)
+            value := calldataload(add(route.offset, previousPtr))
+        }
+    }
+
+    function decode(bytes32 value)
         internal
         pure
-        returns (uint256 previousPtr, address pair, address token, uint256 flags)
-    {
-        unchecked {
-            previousPtr = ptr - 44;
-        }
-        (pair, token, flags) = _get(route, previousPtr);
-    }
-
-    function _get(bytes calldata route, uint256 ptr)
-        private
-        pure
-        returns (address pair, address token, uint256 flags)
+        returns (address pair, uint256 percent, uint256 flags, uint256 tokenInId, uint256 tokenOutId)
     {
         assembly {
-            pair := shr(96, calldataload(add(route.offset, ptr)))
-
-            let value := calldataload(add(route.offset, add(ptr, 20)))
-
-            token := shr(96, value)
-            flags := and(shr(64, value), 0xffffffff)
+            pair := shr(ADDRESS_SHIFT, value)
+            percent := and(shr(PERCENT_SHIFT, value), UINT16_MASK)
+            flags := and(shr(FLAGS_SHIFT, value), UINT16_MASK)
+            tokenInId := and(shr(TOKEN_IN_SHIFT, value), UINT8_MASK)
+            tokenOutId := and(shr(TOKEN_OUT_SHIFT, value), UINT8_MASK)
         }
     }
 }
--- test/libraries/PackedRoute.t.sol
+++ test/libraries/PackedRoute.t.sol
@@ -12,59 +12,172 @@ contract PackedRouteTest is Test {
         lib = new PackedRouteLib();
     }
 
-    function test_Fuzz_Length(uint256 l) public view {
-        l = bound(l, 0, 10);
+    function test_Fuzz_Start(uint256 nbTokens, uint256 nbSwaps) public view {
+        nbTokens = bound(nbTokens, 0, 255);
+        nbSwaps = bound(nbSwaps, 0, 255);
 
-        bytes memory route = new bytes(16 + l * 44);
+        bytes memory route = abi.encodePacked(
+            uint8(nbTokens),
+            new bytes(
+                PackedRoute.TOKENS_OFFSET - 1 + PackedRoute.ADDRESS_SIZE * nbTokens + PackedRoute.ROUTE_SIZE * nbSwaps
+            )
+        );
 
-        assertEq(lib.length(route), l, "test_Fuzz_Length::1");
+        (uint256 ptr, uint256 nbTokens_, uint256 nbSwaps_) = lib.start(route);
+
+        assertEq(ptr, PackedRoute.TOKENS_OFFSET + PackedRoute.ADDRESS_SIZE * nbTokens, "test_Fuzz_Start::1");
+        assertEq(nbTokens_, nbTokens, "test_Fuzz_Start::2");
+        assertEq(nbSwaps_, nbSwaps, "test_Fuzz_Start::3");
     }
 
-    function test_Fuzz_Revert_Length(uint256 l) public {
-        l = bound(l, 0, 256);
+    function test_Fuzz_Revert_Start(uint256 nbTokens, uint256 length) public {
+        nbTokens = bound(nbTokens, 0, 255);
+        uint256 minLength = PackedRoute.TOKENS_OFFSET - 1 + PackedRoute.ADDRESS_SIZE * nbTokens;
 
-        if (l % 44 == 16) ++l;
+        uint256 badLength = bound(length, 0, minLength - 1);
+        bytes memory route = abi.encodePacked(uint8(nbTokens), new bytes(badLength));
 
-        bytes memory route = new bytes(l);
         vm.expectRevert(PackedRoute.PackedRoute__InvalidLength.selector);
-        lib.length(route);
+        lib.start(route);
+
+        badLength = bound(length, minLength + 1, minLength + 2048);
+        badLength = (badLength - minLength) % PackedRoute.ROUTE_SIZE != 0 ? badLength : badLength + 1;
+
+        route = abi.encodePacked(uint8(nbTokens), new bytes(badLength));
+
+        vm.expectRevert(PackedRoute.PackedRoute__InvalidLength.selector);
+        lib.start(route);
     }
 
-    function test_Fuzz_AmountNextPrevious(bytes calldata route) external pure {
-        vm.assume(route.length >= 16 + 44);
+    function test_Fuzz_IsTransferTax(bool isTransferTax, bytes memory data) public view {
+        bytes memory route = abi.encodePacked(uint8(0), isTransferTax, data);
 
-        (uint256 ptr, uint256 amount) = PackedRoute.start(route);
+        assertEq(lib.isTransferTax(route), isTransferTax, "test_Fuzz_IsTransferTax::1");
+    }
 
-        assertEq(ptr, 16, "test_Fuzz_AmountNextPrevious::1");
-        assertEq(amount, uint128(bytes16(route[0:16])), "test_Fuzz_AmountNextPrevious::2");
-
-        address token;
-        address pair;
-        uint256 nextPtr;
-        uint256 flags;
-
-        for (; ptr + 44 <= route.length; ptr += 44) {
-            (nextPtr, pair, token, flags) = PackedRoute.next(route, ptr);
-
-            assertEq(nextPtr, ptr + 44, "test_Fuzz_AmountNextPrevious::3");
-            assertEq(pair, address(bytes20(route[ptr:ptr + 20])), "test_Fuzz_AmountNextPrevious::4");
-            assertEq(token, address(bytes20(route[ptr + 20:ptr + 40])), "test_Fuzz_AmountNextPrevious::5");
-            assertEq(flags, uint32(bytes4(route[ptr + 40:ptr + 44])), "test_Fuzz_AmountNextPrevious::6");
+    function test_Fuzz_NextPrevious(address[] memory tokens, bytes26[] memory details) public view {
+        if (tokens.length > 32) {
+            assembly {
+                mstore(tokens, 32)
+            }
         }
 
-        for (; ptr >= 44;) {
-            (nextPtr, pair, token, flags) = PackedRoute.previous(route, ptr);
+        if (details.length > 32) {
+            assembly {
+                mstore(details, 32)
+            }
+        }
 
-            assertEq(nextPtr, ptr -= 44, "test_Fuzz_AmountNextPrevious::7");
-            assertEq(pair, address(bytes20(route[ptr:ptr + 20])), "test_Fuzz_AmountNextPrevious::8");
-            assertEq(token, address(bytes20(route[ptr + 20:ptr + 40])), "test_Fuzz_AmountNextPrevious::9");
-            assertEq(flags, uint32(bytes4(route[ptr + 40:ptr + 44])), "test_Fuzz_AmountNextPrevious::10");
+        uint256 length = PackedRoute.ADDRESS_SIZE * tokens.length + PackedRoute.ROUTE_SIZE * details.length;
+
+        bytes memory route = abi.encodePacked(uint8(tokens.length), uint8(0), new bytes(length));
+
+        unchecked {
+            uint256 ptr_ = PackedRoute.TOKENS_OFFSET;
+            for (uint256 i = 0; i < tokens.length; i++) {
+                assembly {
+                    mstore(add(add(route, 32), ptr_), shl(96, mload(add(tokens, add(32, mul(i, 32))))))
+                }
+                ptr_ += PackedRoute.ADDRESS_SIZE;
+            }
+
+            for (uint256 i = 0; i < details.length; i++) {
+                assembly {
+                    mstore(add(add(route, 32), ptr_), mload(add(details, add(32, mul(i, 32)))))
+                }
+                ptr_ += PackedRoute.ROUTE_SIZE;
+            }
+        }
+
+        this._nextPrevious(tokens, details, route);
+    }
+
+    function _nextPrevious(address[] calldata tokens, bytes26[] calldata details, bytes calldata route) public pure {
+        (uint256 ptr, uint256 nbTokens, uint256 nbSwaps) = PackedRoute.start(route);
+
+        assertEq(nbTokens, tokens.length, "test_Fuzz_NextPrevious::1");
+        assertEq(nbSwaps, details.length, "test_Fuzz_NextPrevious::2");
+        assertEq(ptr, PackedRoute.TOKENS_OFFSET + PackedRoute.ADDRESS_SIZE * nbTokens, "test_Fuzz_NextPrevious::1");
+
+        uint256 startPtr = ptr;
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            assertEq(PackedRoute.token(route, i), tokens[i], "test_Fuzz_NextPrevious::3");
+        }
+
+        bytes32 value;
+        for (uint256 i = 0; i < details.length; i++) {
+            (ptr, value) = PackedRoute.next(route, ptr);
+
+            assertEq(ptr, startPtr + PackedRoute.ROUTE_SIZE * (i + 1), "test_Fuzz_NextPrevious::4");
+            assertEq(
+                value & 0xffffffffffffffffffffffffffffffffffffffffffffffffffff000000000000,
+                details[i],
+                "test_Fuzz_NextPrevious::5"
+            );
+
+            (address pair, uint256 percent, uint256 flags, uint256 tokenInId, uint256 tokenOutId) =
+                PackedRoute.decode(value);
+
+            uint256 detail = uint256(bytes32(details[i]));
+
+            assertEq(pair, address(uint160(detail >> 96)), "test_Fuzz_NextPrevious::6");
+            assertEq(percent, uint16(detail >> 80), "test_Fuzz_NextPrevious::7");
+            assertEq(flags, uint16(detail >> 64), "test_Fuzz_NextPrevious::8");
+            assertEq(tokenInId, uint8(detail >> 56), "test_Fuzz_NextPrevious::9");
+            assertEq(tokenOutId, uint8(detail >> 48), "test_Fuzz_NextPrevious::10");
+        }
+
+        for (uint256 i = details.length; i > 0; i--) {
+            (ptr, value) = PackedRoute.previous(route, ptr);
+
+            assertEq(ptr, startPtr + PackedRoute.ROUTE_SIZE * (i - 1), "test_Fuzz_NextPrevious::11");
+            assertEq(
+                value & 0xffffffffffffffffffffffffffffffffffffffffffffffffffff000000000000,
+                details[i - 1],
+                "test_Fuzz_NextPrevious::12"
+            );
+
+            (address pair, uint256 percent, uint256 flags, uint256 tokenInId, uint256 tokenOutId) =
+                PackedRoute.decode(value);
+
+            uint256 detail = uint256(bytes32(details[i - 1]));
+
+            assertEq(pair, address(uint160(detail >> 96)), "test_Fuzz_NextPrevious::13");
+            assertEq(percent, uint16(detail >> 80), "test_Fuzz_NextPrevious::14");
+            assertEq(flags, uint16(detail >> 64), "test_Fuzz_NextPrevious::15");
+            assertEq(tokenInId, uint8(detail >> 56), "test_Fuzz_NextPrevious::16");
+            assertEq(tokenOutId, uint8(detail >> 48), "test_Fuzz_NextPrevious::17");
         }
     }
 }
 
 contract PackedRouteLib {
-    function length(bytes calldata route) external pure returns (uint256) {
-        return PackedRoute.length(route);
+    function token(bytes calldata route, uint256 id) external pure returns (address t) {
+        return PackedRoute.token(route, id);
+    }
+
+    function isTransferTax(bytes calldata route) external pure returns (bool b) {
+        return PackedRoute.isTransferTax(route);
+    }
+
+    function start(bytes calldata route) external pure returns (uint256 ptr, uint256 nbTokens, uint256 nbSwaps) {
+        return PackedRoute.start(route);
+    }
+
+    function next(bytes calldata route, uint256 ptr) external pure returns (uint256 nextPtr, bytes32 value) {
+        return PackedRoute.next(route, ptr);
+    }
+
+    function previous(bytes calldata route, uint256 ptr) external pure returns (uint256 previousPtr, bytes32 value) {
+        return PackedRoute.previous(route, ptr);
+    }
+
+    function decode(bytes32 value)
+        external
+        pure
+        returns (address pair, uint256 percent, uint256 flags, uint256 tokenInId, uint256 tokenOutId)
+    {
+        return PackedRoute.decode(value);
     }
 }
